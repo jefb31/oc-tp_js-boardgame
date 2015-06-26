4b0aa1d319ead34d5b3e3173ca012e40256eca7c
Maxence Lagalle <lagalle.maxence@gmail.com>
2015-06-26 22:04:23 +0200

    Création d'un système primitif de remplissage de la grille

--- classes/board.js
+++ classes/board.js
@@ -1,4 +1,11 @@
 // JavaScript Document
 function Board(width, height, probAccessibility) {
 	"use strict";
+	this.grid = new Array(new Array());
+	
+	for(var i = 0; i < width; i++) {
+		for(var j = 0;j < height; j++) {
+			this.grid[i][j] = new Cell(i, j, true);
+		}
+	}
 }
