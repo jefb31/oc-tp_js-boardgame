ddc4b1c5e4e3e538850905f89d43e43b8af86981
Maxence Lagalle <lagalle.maxence@gmail.com>
2015-06-26 22:19:13 +0200

    Système primitif d'initialisation du plateau de jeu

--- classes/board.js
+++ classes/board.js
@@ -3,9 +3,10 @@ function Board(width, height, probAccessibility) {
 	"use strict";
 	this.grid = new Array(new Array());
 	
-	for(var i = 0; i < width; i++) {
-		for(var j = 0;j < height; j++) {
-			this.grid[i][j] = new Cell(i, j, true);
+	for(var i = 1; i <= width; i++) {
+		this.grid[i - 1] = [];
+		for(var j = 1;j <= height; j++) {
+			this.grid[i - 1][j - 1] = new Cell(i, j, true);
 		}
 	}
 }
--- game-controller.js
+++ game-controller.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var gameBoard;
+
+function startNewGame() {
+	"use strict";
+	gameBoard = new Board(10,10,0.9);	
+}
+
+$(document).ready("button[name='newGame']").click(startNewGame);
