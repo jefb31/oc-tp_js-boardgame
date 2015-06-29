89df5f5094ebf7aced5e00b4e63a772857f9799b
Maxence Lagalle <lagalle.maxence@gmail.com>
2015-06-29 10:40:58 +0200

    Mémorisation de la longueur et de la largeur du plateau de jeu comme propriétés

--- classes/board.js
+++ classes/board.js
@@ -1,11 +1,13 @@
 // JavaScript Document
 function Board(width, height, probAccessibility) {
 	"use strict";
+	this.width = width;
+	this.height = height;
 	this.grid = new Array(new Array());
 	
-	for(var i = 1; i <= width; i++) {
+	for(var i = 1; i <= this.width; i++) {
 		this.grid[i - 1] = [];
-		for(var j = 1;j <= height; j++) {
+		for(var j = 1;j <= this.height; j++) {
 			this.grid[i - 1][j - 1] = new Cell(i, j, probAccessibility);
 		}
 	}
