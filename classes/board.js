c0041bf1a3dae13c5839e1e9390b029016eba6d6
Maxence Lagalle <lagalle.maxence@gmail.com>
2015-06-29 12:15:22 +0200

    Refactoring - Génération du tableau en IIFE

--- classes/board.js
+++ classes/board.js
@@ -3,12 +3,14 @@ function Board(width, height, probAccessibility) {
 	"use strict";
 	this.width = width;
 	this.height = height;
-	this.grid = new Array(new Array());
-	
-	for(var i = 1; i <= this.width; i++) {
-		this.grid[i - 1] = [];
-		for(var j = 1;j <= this.height; j++) {
-			this.grid[i - 1][j - 1] = new Cell(i - 1, j - 1, probAccessibility);
+	this.grid = (function(width, height) {
+		var grid = new Array(new Array());
+		for(var i = 1; i <= width; i++) {
+			grid[i - 1] = [];
+			for(var j = 1;j <= height; j++) {
+				grid[i - 1][j - 1] = new Cell(i - 1, j - 1, probAccessibility);
+			}
 		}
-	}
+		return grid;
+	})(width, height);
 }
