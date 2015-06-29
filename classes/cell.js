5b012a95f15636f526f7fb32713613278231fb2a
Maxence Lagalle <lagalle.maxence@gmail.com>
2015-06-29 10:15:04 +0200

    Création d'une variage trigger pour la situation de combat

--- classes/cell.js
+++ classes/cell.js
@@ -3,5 +3,6 @@ function Cell(rowIndex, colIndex, probAccessibility) {
 	"use strict";
 	this.row = rowIndex;
 	this.col = colIndex;
+	this.triggerCombat = false;
 	this.accessible = Math.random() < probAccessibility ? true : false;
 }
