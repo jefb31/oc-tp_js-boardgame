fd9421758e03a5925181b5946e2868cfcf7e2cff
Maxence Lagalle <lagalle.maxence@gmail.com>
2015-06-30 14:19:21 +0200

    Ajout d'icônes pour représenter les armes

--- game/game-engine.js
+++ game/game-engine.js
@@ -2,11 +2,11 @@
 function Game(boardWidth, boardHeight, boardAccessibility, weaponAvailability, maxBonusWeapons) {
 	"use strict";
 	this.board = new Board(boardWidth, boardHeight, boardAccessibility);
-	var baseWeapons = [new Weapon(1, "Red Blaster", 10),
-						new Weapon(2, "Blue Laser", 10)]
+	var baseWeapons = [new Weapon(1, "Poing Gauche", 10),
+						new Weapon(2, "Poing Droit", 10)]
 	var bonusWeapons = [new Weapon(3, "Petite Aiguille", 5),
-						new Weapon(4, "Grande Martha", 50),
-						new Weapon(5, "Double Laser", 20),
+						new Weapon(4, "Grande Faucille", 50),
+						new Weapon(5, "Arc de Robin", 20),
 						new Weapon(6, "Calibre 35", 35)];
 	this.players = [new Player(1, baseWeapons[0], this.board), new Player(2, baseWeapons[1], this.board)];
 			
